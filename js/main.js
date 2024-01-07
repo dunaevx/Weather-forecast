16abf7931605a83e5fb98ac263a8c129e08b47d5
dunaevx <hamiddunaev@gmail.com>
2024-01-07 22:28:33 +0600

    визуал готов, осталось логика сайта и API

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-// Проверяем поддержку геолокации в браузере
-if (navigator.geolocation) {
-    // Запрашиваем геолокацию
-    navigator.geolocation.getCurrentPosition(
-      // Функция обратного вызова при успешном получении координат
-      function(position) {
-        console.log(position)
-      },
-      // Функция обратного вызова при ошибке
-      function(error) {
-        console.error(`Ошибка получения геолокации: ${error.message}`);
-      }
-    );
-  } else {
-    console.log("Геолокация не поддерживается в вашем браузере.");
-  }
-  
+// let Key = ce822fbd27b54e0c8b394753240701
+
+// http://api.weatherapi.com/v1/current.json?key=ce822fbd27b54e0c8b394753240701&q=London
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  let num1 = document.getElementById('number1');
+  let counter1 = 0;
+
+  setInterval(()=>{
+    if(counter1 === 65){
+      clearInterval()
+    }else{
+      counter1 += 1;
+      num1.innerHTML = counter1 + "%";
+    }
+
+  }, 15)
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  let num2 = document.getElementById('number2');
+  let counter2 = 0;
+
+  setInterval(()=>{
+    if(counter2 === 85){
+      clearInterval()
+    }else{
+      counter2 += 1;
+      num2.innerHTML = counter2 + "%";
+    }
+
+  }, 12)
+
+});
