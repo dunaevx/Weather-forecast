b0a6c78b567a1b71c2c39b006d6eb3b9fe77e9e6
dunaevx <hamiddunaev@gmail.com>
2024-01-09 17:45:28 +0600

    Добавлена синхронизация таймера между анимацией и процессом загрузки в проекте.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,27 @@
 
 // http://api.weatherapi.com/v1/current.json?key=ce822fbd27b54e0c8b394753240701&q=London
 
+const dashoffsetOne = (100 - 50) / 100 * 307;  // чилсо 50 заменится на переменную
+        document.documentElement.style.setProperty('--custom-dashoffset-One', dashoffsetOne);
+const dashoffsetSec = (100 - 85) / 100 * 307; // чилсо 85 заменится на переменную
+        document.documentElement.style.setProperty('--custom-dashoffset-Sec', dashoffsetSec);
+
+const timeCircleOne = 1000/50;  // чилсо 50 заменится на переменную
+const timeCircleSec = 1000/85;  // чилсо 85 заменится на переменную
 
 document.addEventListener('DOMContentLoaded', () => {
   let num1 = document.getElementById('number1');
   let counter1 = 0;
 
   setInterval(()=>{
-    if(counter1 === 65){
+    if(counter1 === 50){  // чилсо 50 заменится на переменную
       clearInterval()
     }else{
       counter1 += 1;
       num1.innerHTML = counter1 + "%";
     }
 
-  }, 15)
+  }, timeCircleOne)
 
 });
 
@@ -24,13 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let counter2 = 0;
 
   setInterval(()=>{
-    if(counter2 === 85){
+    if(counter2 === 85){  // чилсо 85 заменится на переменную
       clearInterval()
     }else{
       counter2 += 1;
       num2.innerHTML = counter2 + "%";
     }
 
-  }, 12)
+  }, timeCircleSec)
 
 });
